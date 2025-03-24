0915887f03687b483bc0377fbc3b24fcb5b42b8e
leo <leoventura06@outlook.it>
2025-03-24 16:06:47 +0100

    setup theme

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import {ThemeProvider} from "@/components/ThemeProvider";
+import {Header} from "@/components/Header";
 
 const inter = Inter({ subsets: [ "latin" ] })
 
@@ -15,11 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <Header />
+            {children}
+        </ThemeProvider>
       </body>
     </html>
   );
